85f73277966d3d5543db941cb65521564ce2429b
daniellekorn <danielleko@wix.com>
2025-02-03 11:23:07 +0200

    add logo

--- my-element.js
+++ my-element.js
@@ -1,7 +1,23 @@
-class MyElement extends HTMLElement {
+const LOGO_IMG = "https://static.wixstatic.com/media/logo-img.png";
+
+const createImg = (id, src) => {
+  const img = document.createElement("img");
+  img.src = src;
+  img.id = id;
+  img.width = "200";
+  img.style.position = "fixed";
+  return img;
+};
+
+class myElement extends HTMLElement {
+  constructor() {
+    super();
+  }
+
   connectedCallback() {
+    this.appendChild(createImg("img", LOGO_IMG));
     this.innerHTML = `<p>Hello from MyElement!</p>`;
   }
 }
 
-customElements.define("my-element", MyElement);
+customElements.define("my-element", myElement);
