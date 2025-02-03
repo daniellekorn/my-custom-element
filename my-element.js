3cf42aba5a07c5846fa580f1d4df204075cd7045
daniellekorn <danielleko@wix.com>
2025-02-03 11:24:33 +0200

    minor fixes

--- my-element.js
+++ my-element.js
@@ -1,23 +1,21 @@
-const LOGO_IMG = "https://static.wixstatic.com/media/logo-img.png";
-
 const createImg = (id, src) => {
   const img = document.createElement("img");
   img.src = src;
   img.id = id;
-  img.width = "200";
+  img.width = 200;
   img.style.position = "fixed";
   return img;
 };
 
-class myElement extends HTMLElement {
+class MyElement extends HTMLElement {
   constructor() {
     super();
   }
 
   connectedCallback() {
-    this.appendChild(createImg("img", LOGO_IMG));
-    this.innerHTML = `<p>Hello from MyElement!</p>`;
+    this.innerHTML = `<p>Hello from MyElement!</p>`; // Set HTML first
+    this.appendChild(createImg("img", "./wix.png")); // Then append image
   }
 }
 
-customElements.define("my-element", myElement);
+customElements.define("my-element", MyElement);
